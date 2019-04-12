fc4bd9611c76a805cdb8ef12e1d42acdb112b051
Gowtham Behara <gothsiddu@gmail.com>
2019-04-12 22:01:43 +0530

    Add problem statement

---
+++ Beginner/SumOfDigits.c
@@ -0,0 +1,15 @@
+/*
+You're given an integer N. Write a program to calculate the sum of all the digits of N.
+
+Input
+The first line contains an integer T, total number of testcases. Then follow T lines, each line contains an integer N.
+
+Output
+Calculate the sum of digits of N.
+
+Constraints
+1 ≤ T ≤ 1000
+1 ≤ N ≤ 1000000
+
+https://www.codechef.com/problems/FLOW006
+*/ 
