84e87b19c16cf46e07f6544a87422515c74e13d0
Gowtham Behara <gothsiddu@gmail.com>
2019-04-14 11:13:56 +0530

    Define func to get pckg size with max leftover

--- Beginner/PackaginCupcakes.c
+++ Beginner/PackaginCupcakes.c
@@ -22,3 +22,7 @@ In the first test case, there will be no leftover cupcakes regardless of the siz
 
 https://www.codechef.com/problems/MUFFINS3
 */
+
+int getPckgSizeWithMaxLeftover(int numOfCupcakes) {
+    return numOfCupcakes / 2 + 1;
+}
