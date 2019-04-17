903354f39d46705e34192cf4b2bc2e4e27207e70
Gowtham Behara <gothsiddu@gmail.com>
2019-04-17 09:44:18 +0530

    Add problem statement

---
+++ Beginner/ValidTriangles.c
@@ -0,0 +1,27 @@
+/*
+Write a program to check whether a triangle is valid or not, when the three angles of the triangle are the inputs. A triangle is valid if the sum of all the three angles is equal to 180 degrees.
+
+Input
+The first line contains an integer T, total number of testcases. Then follow T lines, each line contains three angles A, B and C of triangle separated by space.
+
+Output
+Display 'YES' or 'NO' if the triangle is Valid or not respectively.
+
+Constraints
+1 ≤ T ≤ 1000
+1 ≤ A,B,C ≤ 180
+Example
+Input
+
+3 
+40 40 100
+45 45 90
+180 1 1
+Output
+
+YES
+YES
+NO
+
+https://www.codechef.com/problems/FLOW013
+*/
