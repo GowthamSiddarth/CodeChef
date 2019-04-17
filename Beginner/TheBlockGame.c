9c8a6bf1f294ca13d7ecbfee37ff31694dc8da99
Gowtham Behara <gothsiddu@gmail.com>
2019-04-17 10:21:16 +0530

    Define func to get reverse of a num

--- Beginner/TheBlockGame.c
+++ Beginner/TheBlockGame.c
@@ -25,3 +25,15 @@ wins
 
 https://www.codechef.com/problems/PALL01
 */
+
+int reverse(int num)
+{
+    int sum = 0;
+    while (num)
+    {
+        sum = sum * 10 + num % 10;
+        num = num / 10;
+    }
+
+    return sum;
+}
