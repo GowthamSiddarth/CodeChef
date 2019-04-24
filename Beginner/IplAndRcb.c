9140ace832535e79a2b5d58f07cbf53f5bc1e0fd
Gowtham Behara <gothsiddu@gmail.com>
2019-04-24 19:21:14 +0530

    Add problem statement

---
+++ Beginner/IplAndRcb.c
@@ -0,0 +1,26 @@
+/*
+It's IPL time and as usual RCB are finding it tough to qualify for playoffs. RCB needs a minimum of X more points to qualify for playoffs in their remaining Y matches. A win, tie and loss in a match will yield 2,1,0 points respectively to a team.
+
+You being a true RCB supporter want to find the minimum number of matches RCB needs to win to qualify for playoffs. It is guaranteed that RCB will qualify for playoffs if they win all their remaining Y matches.
+
+Input:
+First line will contain T, number of testcases. Then the testcases follow.
+Each testcase contains of a single line of input, two space separated integers X,Y
+Output:
+For each testcase, output in a single line the minimum number of matches RCB must win to qualify for playoffs.
+
+Constraints :
+1≤T≤10000
+1≤X≤100
+1≤Y≤100
+1≤X≤2⋅Y
+Sample Input:
+2
+10 5
+1 5
+Sample Output:
+5
+0
+
+https://www.codechef.com/problems/CLIPLX
+*/
